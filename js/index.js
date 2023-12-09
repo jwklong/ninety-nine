6cc4fd5c7abe40bf2854600b57b4ee38dd1eb90f
sussy layers dev <jwklong@gmx.com>
2023-12-09 19:37:55 +0000

    stick

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import { Items } from "./items.js";
 
-for (var item of Items.items) {
+for (var item of Items.items.sort((a, b) => b.amount - a.amount)) {
     if (item.amount > 0) {
         var el = item.generate()
         document.getElementById('items').appendChild(el)
--- js/items.js
+++ js/items.js
@@ -4,8 +4,10 @@ import Item from "./classes/item.js"
 export const Items = new Inventory
 
 Items.addItem(new Item('Rock', 'rock', 'assets/images/rock.svg', 'Something to throw at a window.'))
+Items.addItem(new Item('Stick', 'stick', 'assets/images/stick.svg', 'Wow.<br>A stick.'))
 Items.addItem(new Item('Orb', 'orb', 'assets/images/orb.svg', '<span style="color: #b4f">???</span>'))
 
 //test
 Items.getItemFromName('Rock').amount += 2
+Items.getItemFromName('Stick').amount += 3
 Items.getItemFromName('Orb').amount += 1
