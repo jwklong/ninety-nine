a4be463b62de4a293e8613a8e03d2b599077a2d9
sussy layers dev <jwklong@gmx.com>
2023-12-10 08:17:50 +0000

    switcher

--- js/index.js
+++ js/index.js
@@ -6,3 +6,14 @@ for (var item of Items.items) {
         document.getElementById('items').appendChild(el)
     }
 }
+
+for (let button of document.getElementById('switcher').children) {
+    button.addEventListener('click', (e) => {
+        for (let tab of document.getElementById('main').children) {
+            tab.style.display = tab.id == button.classList.value ? "" : "none"
+        }
+    })
+}
+for (let tab of document.getElementById('main').children) {
+    tab.style.display = tab.id == "inventory" ? "" : "none"
+}
